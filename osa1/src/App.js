7ddcca84b24fe4a4fdb1afb2620ed6781c2ac5fb
Gena <genci.behxheti@gmail.com>
2023-05-18 20:40:36 +0300

    osa 2.5 done

--- osa1/src/App.js
+++ osa1/src/App.js
@@ -1,44 +1,4 @@
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Content = ({ course }) => {
-  const [parts] = [course.parts]
-  console.log(parts)
-  
-  return (
-    <div>
-      {parts.map(item => <Part key={item.id} part={item} />)}
-      <Total parts={parts} />
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-
-  return (
-    <p>Total exercises: {parts.reduce((sum, part) => sum + part.exercises, 0)} </p>
-  )
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
+import Course from './Course.js'
 
 const App = () => {
   const course = {
