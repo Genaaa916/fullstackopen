2bd7f711d7bfa6cdd52c2f9096067263c95fb7b6
Gena <genci.behxheti@gmail.com>
2023-05-20 21:44:20 +0300

    -

--- osa1/src/App.js
+++ osa1/src/App.js
@@ -2,155 +2,66 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 
-const Form = ({ functions }) => {
-  const [addPerson, handleNameChange, handleNumberChange] = functions;
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+//cors not enabled on the server ??
+//not finished
 
-const Phonebook = ({ persons, deletePerson }) => {
+const Search = ({ search, setSearch }) => {
   return (
     <div>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div>
-          {" "}
-          <p className="person" key={person.id}>
-            {person.name} - {person.number}
-          </p>
-          <button onClick={() => deletePerson(person)}>Delete</button>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const Filter = ({ filterNames }) => {
-  return (
-    <div>
-      Search: <input onChange={filterNames} />
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </div>
   );
 };
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
-  };
-
-  const baseUrl = "http://localhost:3001/";
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    getAll();
+    const getRidOfCors = "https://cors-anywhere.herokuapp.com/";
+    axios
+      .get(
+        `${getRidOfCors}https://studies.cs.helsinki.fi/restcountries/api/all`,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
   }, []);
 
-  const getAll = async () => {
-    try {
-      const response = await axios.get(`${baseUrl}persons`);
-      setPersons(response.data);
-      setError(null);
-      console.log(persons);
-    } catch (err) {
-      setError(err);
-      console.log(error);
-    }
-  };
-  const handleNumberChange = (e) => {
-    setNewNumber(e.target.value);
-  };
-
-  const filterNames = (e) => {
-    const searchValue = e.target.value.toLowerCase();
-
-    const filteredPersons = persons.filter((i) =>
-      i.name.toLowerCase().includes(searchValue)
-    );
-
-    setPersons(filteredPersons);
-    console.log(filteredPersons);
-  };
-
-  const updatePerson = async (person) => {
-    window.confirm(`${person.name} is already added, replace the old number?`);
-    try {
-      const updatedPerson = { ...person, number: newNumber };
-      await axios.put(`${baseUrl}persons/${person.id}`, updatedPerson);
-      setPersons(persons.map((i) => (i.id !== person.id ? i : updatedPerson)));
-    } catch (err) {
-      setError(err.response);
-      console.log(err.response);
-    }
-  };
-
-  const postPerson = async (newPerson) => {
-    try {
-      await axios.post(`${baseUrl}persons`, newPerson);
-      setPersons(persons.concat(newPerson));
-    } catch (err) {
-      setError(err.response);
-      console.log("error");
-    }
-  };
-  const addPerson = (e) => {
-    e.preventDefault();
-    const newPerson = {
-      name: newName,
-      id: persons.length + 1,
-      number: newNumber,
-    };
-    persons.alreadyExists = persons.find((i) => i.name === newName);
-    persons.alreadyExists
-      ? updatePerson(persons.find((i) => i.name === newName))
-      : postPerson(newPerson);
-    setNewNumber("");
-    setNewName("");
-    e.target.reset();
-  };
-
-  const deletePerson = async (person) => {
-    try {
-      await axios.delete(`${baseUrl}persons/${person.id}`);
-      setPersons(persons.filter((i) => i.id !== person.id));
-      setError(null);
-    } catch (err) {
-      setError(err.response);
-    }
-  };
+  loading && <div>Loading...</div>;
+  error && <div>Error...</div>;
 
   return (
     <div>
-      <Filter filterNames={filterNames} />
-      <Form functions={[addPerson, handleNameChange, handleNumberChange]} />
-      {!error ? (
-        <Phonebook deletePerson={deletePerson} persons={persons} />
-      ) : (
-        <div className="error">
-          <p>Something went wrong</p>
-          <p>{error.status === 404 ? "Not found" : error.message}</p>
-          <button onClick={addPerson}>Try again</button>
-        </div>
-      )}
+      <Search setSearch={setSearch} search={search} />
+      {data &&
+        data.map((country) => {
+          return (
+            <div key={country.name.official}>
+              <h1>{country.name.official}</h1>
+              <p>Capital: {country.capital}</p>
+              <p>Population: {country.population}</p>
+              <img src={country.flags.png} alt="flag" width="100px" />
+            </div>
+          );
+        })}
     </div>
   );
 };
-
 export default App;
